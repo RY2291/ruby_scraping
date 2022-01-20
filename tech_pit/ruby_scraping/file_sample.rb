45dbe3ecc5f913c30e6307540735cb20c2ceb2d9
RY2291 <prgr2498@gmail.com>
2022-01-20 22:34:43 +0900

    [fin]取得したHTMLをファイルに保存

---
+++ tech_pit/ruby_scraping/file_sample.rb
@@ -0,0 +1,9 @@
+# file = File.open("scrape.rb")
+# puts file.read
+# file.close
+
+file = File.open("hello.txt", "w")
+file.write("Hello World")
+file.close
+
+file = File.open("")
--- tech_pit/ruby_scraping/scrape.rb
+++ tech_pit/ruby_scraping/scrape.rb
@@ -3,4 +3,7 @@ require "net/http"
 url = 'https://masayuki14.github.io/pit-news/'
 uri = URI(url)
 html = Net::HTTP.get(uri)
-puts html
+
+file = File.open("pitnews.html", "w")
+file.write(html)
+file.close
