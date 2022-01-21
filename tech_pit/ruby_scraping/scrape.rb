b6e325bf0da7afbfb56950efa5f1fdce65b05f72
RY2291 <prgr2498@gmail.com>
2022-01-21 12:34:15 +0900

    [fin]3章_リファクタリング

--- tech_pit/ruby_scraping/scrape.rb
+++ tech_pit/ruby_scraping/scrape.rb
@@ -1,5 +1,6 @@
 require "net/http"
 require "nokogiri"
+require "json"
 
 
 def get_from(url)
@@ -7,32 +8,27 @@ def get_from(url)
 end
 
 def write_file(path, text)
-  File.open(path, "w") { |file| file.write({text: pintnews}.to_json) }
+  File.open(path, "w") { |file| file.write(text) }
 end
 
-html = File.open("pintnews.html", "r") { |f| f.read }
+def scrape_news(news)
+  {
+    title: news.xpath("./p/strong/a").first.text,
+    url: news.xpath("./p/strong/a").first["href"]
+  }
+end
+
+def scrape_section(section)
+  {
+    category: section.xpath("./h6").first.text,
+    news: section.xpath("./div/div").map { |node| scrape_news(node) }
+  }
+end
+
+html = File.read('pintnews.html')
 doc = Nokogiri::HTML.parse(html, nil, "utf-8")
-
-pintnews = []
-doc.xpath("/html/body/main/section").each_with_index do |section, index|
-
-  next if index.zero?
-  contents = { category: nil, news: [] }
-  contents[:category] = section.xpath("./h6").first.text
-
-  section.xpath("./div/div").each do |node|
-    title = node.xpath("./p/strong/a").first.text
-    url = node.xpath("./p/strong/a").first["href"]
-
-    news = {title: title, url: url}
-    contents[:news] << news
-  end
-
-  pintnews << contents
-end
-
-require "json"
-File.open("pintnews.json", "w") { |file| file.write({ pintnews: pintnews}.to_json)}
+pintnews = doc.xpath("/html/body/main/section[position() > 1]").map { |section| scrape_section(section)}
+write_file("pintnews.json", { pintnews: pintnews}.to_json)
 
 
 # # //はルートノード以下の全要素が対象になる
